6fc30dbe2e7b0d4323bbd0e7411c418cd8cf57ff
Pete Dring <dringp@fulford.york.sch.uk>
2026-07-17 10:13:54 +0100

    Implement modes for sensor data transmission control
    Added modes for temporarily and toggled on states for sensor data transmission. Updated button handling for mode changes and device ID toggling.
    Closes #1

--- microbit/sensor.py
+++ microbit/sensor.py
@@ -13,22 +13,46 @@ image_send = Image("99900:"
                    "00909:"
                    "90909")
 
+# modes:
+MODE_OFF = 0
+MODE_TEMPORARILY_ON = 1
+MODE_TOGGLED_ON = 2
+mode = MODE_OFF
 while True:
     # Press and hold button a to transmit accelerometer data
     if button_a.is_pressed():
-        display.show(image_send)
-        x = accelerometer.get_x()
-        y = accelerometer.get_y()
-        z = accelerometer.get_z()
-        msg = "{} {} {} {}".format(id_str, x,y,z)
-        radio.send(msg)
-        print(msg)
-    
+        mode = MODE_TEMPORARILY_ON
+        if button_b.is_pressed():
+            mode = MODE_TOGGLED_ON
+            display.show(Image.YES)
+            sleep(500)
+            
+            # keep holding both buttons to turn sending back off
+            if button_b.was_pressed() and button_a.is_pressed():
+                display.show(Image.NO)
+                sleep(500)
+                mode = MODE_OFF
     else:
+        if mode == MODE_TEMPORARILY_ON:
+            mode = MODE_OFF
+        
         # press button b to toggle device ID
         if button_b.was_pressed():
             id_int = (id_int + 1) % len(ids)
             id_str = ids[id_int]
         display.show(id_str)
+        
+    # transmit accelerometer values unless in OFF mode
+    if mode > 0:
+        display.show(image_send)
+        x = accelerometer.get_x()
+        y = accelerometer.get_y()
+        z = accelerometer.get_z()
+        msg = "{} {} {} {}".format(id_str, x,y,z)
+        
+        # send via radio
+        radio.send(msg)
+        
+        # output to REPL (for debugging)
+        print(msg)
     sleep(10)
-
